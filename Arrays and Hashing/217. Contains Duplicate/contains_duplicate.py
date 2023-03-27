367a5a5e58aa3a887d9d280192e7ed9117ad6388
Christian Lam <christian.lam@csesoc.org.au>
2023-03-28 10:19:22 +1100

    refactor: Uncomment all sols

--- Arrays and Hashing/217. Contains Duplicate/contains_duplicate.py
+++ Arrays and Hashing/217. Contains Duplicate/contains_duplicate.py
@@ -2,14 +2,14 @@
 # 2. Alternative Solution
 
 # 1. Naiive Solution
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         s = set()
-#         for num in nums:
-#             if num in s:
-#                 return True
-#             s.add(num)
-#         return False
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            s.add(num)
+        return False
         
 # 2. Alternative Solution
 class Solution:
